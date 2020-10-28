b4f4e68462324c27ec045027b872695d1e037f51
VFC <12@3.4>
2020-10-28 11:56:12 +0800

    回头再玩

--- Routiner.py
+++ Routiner.py
@@ -8,18 +8,40 @@ from graia.broadcast.entities.decorater import Decorater
 from graia.broadcast.builtin.decoraters import Depend, Middleware
 from graia.broadcast.interfaces.decorater import DecoraterInterface
 from graia.broadcast.exceptions import PropagationCancelled
+from graia.application.event.messages import GroupMessage
 import random
 import asyncio
 import time
 import copy
 import datetime
 
+# class GroupMessage(MiraiEvent):
+#     type: str = "GroupMessage"
+#     messageChain: MessageChain
+#     sender: Member
 
+#     class Dispatcher(BaseDispatcher):
+#         mixin = [MessageChainCatcher, ApplicationDispatcher]
+
+#         @staticmethod
+#         def catch(interface: DispatcherInterface):
+#             if interface.annotation is Group:
+#                 return interface.event.sender.group
+#             elif interface.annotation is Member:
+#                 return interface.event.sender
+
+class parseminus(BaseDispatcher):
+    @staticmethod
+    async def catch(interface: DispatcherInterface):
+        print(interface.name)
+        if interface.name == "extDict":
+            print('etx')
+            return {'a':233}
 
 class D1(BaseDispatcher):
     @staticmethod
     def catch(interface: DispatcherInterface):
-        if interface.annotation == "123":
+        if interface.annotation == dict:
             return random.random()
 
 class D2(BaseDispatcher):
@@ -31,11 +53,14 @@ class D2(BaseDispatcher):
             return r
 
 class TestEvent(BaseEvent):
+    extDict: dict
+
     class Dispatcher(BaseDispatcher):
         mixin = [D2]
 
         @staticmethod
         def catch(interface: DispatcherInterface):
+            # print(interface.parameter_contexts)
             if interface.name == "u":
                 yield 1
             elif interface.annotation == str:
@@ -54,14 +79,22 @@ async def r(extDict: dict):
         i += 1
 
 # for _ in range(15):
-@broadcast.receiver(TestEvent, headless_decoraters=[Depend(r)])
-async def test(extDict: dict):
+@broadcast.receiver(TestEvent, headless_decoraters=[Depend(r)], dispatchers=[parseminus])
+async def test():
     print(datetime.datetime.now())
         # print(extDict)
 
+@broadcast.receiver(TestEvent, headless_decoraters=[Depend(r)])
+async def test2(extDict: dict):
+    print(extDict)
+
+@broadcast.receiver(TestEvent, headless_decoraters=[Depend(r)])
+async def test3():
+    print('coconmsl')
+
 async def main(start):
-    print("将在 5 s 后开始测试.")
-    for i in range(1, 6):
+    print("将在 1 s 后开始测试.")
+    for i in range(1, 2):
         print(i)
         await asyncio.sleep(1)
     print("测试开始.", start)
@@ -74,8 +107,8 @@ start = time.time()
 loop.run_until_complete(main(start))
 
 end = time.time()
-print(f"测试结束, 15s 后退出, 用时 {end - start - 5}")
-loop.run_until_complete(asyncio.sleep(15))
+print(f"测试结束, 1s 后退出, 用时 {end - start - 5}")
+loop.run_until_complete(asyncio.sleep(1))
 
 print(i)
 #import pdb; pdb.set_trace()
