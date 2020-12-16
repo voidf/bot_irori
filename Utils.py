3fb565bdc91e1ad8380a1ddb52ec7fbc218d93aa
Kaguya <kaguya@Kaguya.cn>
2020-12-16 15:46:52 +0800

    codec

--- Utils.py
+++ Utils.py
@@ -442,14 +442,14 @@ def limitAudioSizeByBitrate(src) -> str:
     dur = os.popen(f'ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 {src}').read()
     dur = float(dur)
     print(dur)
-    os.system(f'ffmpeg -y -i {src} -ac 1 -ar 8000 -b:a {lim / dur}k {dst}')
+    os.system(f'ffmpeg -y -i {src} -codec amr_nb -ac 1 -ar 8000 -b:a {lim / dur}k {dst}')
     asyncio.ensure_future(rmTmpFile(dst))
     return dst
 
 def limitAudioSizeByCut(src) -> str:
     """超出部分会被剪掉"""
     dst = generateTmpFileName(ext='.amr')
-    os.system(f'ffmpeg -y -i {src} -ac 1 -ar 8000 -fs 1000K {dst}')
+    os.system(f'ffmpeg -y -i {src} -codec amr_nb -ac 1 -ar 8000 -fs 1000K {dst}')
     asyncio.ensure_future(rmTmpFile(dst))
     return dst
 
