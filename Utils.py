d0a034f84b1f92cb1c27c0caea5189d4f63f0a2d
Kaguya <kaguya@Kaguya.cn>
2020-12-23 19:09:03 +0800

    sbl

--- Utils.py
+++ Utils.py
@@ -461,7 +461,7 @@ def TTS(text, voice='slt') -> str:
         'slt'
     } else 'slt'
     dst = generateTmpFileName(ext='.amr')
-    t = "'" if "'" in text else ""
+    t = "" if "'" in text else "'"
     print(f'''ffmpeg -f lavfi -i flite=text={t}{shlex.quote(text)}{t}:voice={shlex.quote(v)} -codec amr_nb -ac 1 -ar 8000 {dst}''')
     os.system(f'''ffmpeg -f lavfi -i flite=text={t}{shlex.quote(text)}{t}:voice={shlex.quote(v)} -codec amr_nb -ac 1 -ar 8000 {dst}''')
     asyncio.ensure_future(rmTmpFile(dst))
