7b00081b4acc9737d9fb9a69f0f133201bc5ce3a
Kaguya <kaguya@Kaguya.cn>
2021-09-10 08:02:48 +0800

    wip

--- celeryconfig.py
+++
@@ -1,9 +0,0 @@
-broker_url = 'pyamqp://guest@localhost//'
-result_backend= "mongodb://127.0.0.1:27017/irori_taskqueue"
-
-task_time_limit = 30
-result_expires = 3600
-
-imports = tuple()
-worker_pool_restarts = True
-# cat /etc/resolv.conf | grep nameserver | awk '{ print $2 }'
