94d7b0377a12951c5f38a92676ca29e8cee4cf44
Kaguya <kaguya@Kaguya.cn>
2022-01-25 21:41:26 +0800

    add: 试试爆栈论坛的重定向stdout解决exec输出问题

--- fapi/utils/syscall.py
+++ fapi/utils/syscall.py
@@ -1,7 +1,19 @@
 from basicutils.network import *
 import os
+from io import StringIO
+from contextlib import redirect_stdout
+# https://stackoverflow.com/questions/3906232/python-get-the-print-output-in-an-exec-statement
 
-async def sys_exec(ent: CoreEntity, args: list): return f"""{exec(' '.join(args))}"""
+async def sys_exec(ent: CoreEntity, args: list): 
+    import fapi
+    import fapi.routers
+    import fapi.models
+    from fapi.models.Routiner import Routiner
+    from fapi.Sessions import SessionManager
+    f = StringIO()
+    with redirect_stdout(f):
+        exec(' '.join(args))
+    return f.getvalue()
 async def sys_eval(ent: CoreEntity, args: list): 
     import fapi
     import fapi.routers
