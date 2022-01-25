01a20c232d192721a1569a722abc0b693f7f70db
Kaguya <kaguya@Kaguya.cn>
2022-01-25 21:32:47 +0800

    add: eval import调用上下文

--- fapi/utils/syscall.py
+++ fapi/utils/syscall.py
@@ -2,7 +2,13 @@ from basicutils.network import *
 import os
 
 async def sys_exec(ent: CoreEntity, args: list): return f"""{exec(' '.join(args))}"""
-async def sys_eval(ent: CoreEntity, args: list): return f"""{eval(' '.join(args))}"""
+async def sys_eval(ent: CoreEntity, args: list): 
+    import fapi
+    import fapi.routers
+    import fapi.models
+    from fapi.models.Routiner import Routiner
+    from fapi.Sessions import SessionManager
+    return f"""{eval(' '.join(args))}"""
 async def sys_run(ent: CoreEntity, args: list): return f"""{os.popen(' '.join(args)).read()}"""
 async def sys_help(ent: CoreEntity, args: list): return '目前仅支持exec, eval, run, send四个命令'
 async def sys_unauthorized(ent: CoreEntity, args: list): return "您没有权限执行此调用"
