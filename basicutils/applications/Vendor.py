bfa2a1323c7dda137a64411a6b6376269fc778da
Kaguya <kaguya@Kaguya.cn>
2022-10-15 01:26:43 +0800

    feat: 自动拆解

--- basicutils/applications/Vendor.py
+++ basicutils/applications/Vendor.py
@@ -1,12 +1,15 @@
 """临时开的转发接口类"""
 import base64
+import collections
 from dataclasses import dataclass
 import enum
 import os
 import pickle
+import string
 import sys
 
 from async_timeout import timeout
+from click import prompt
 
 if os.getcwd() not in sys.path:
     sys.path.append(os.getcwd())
@@ -27,12 +30,11 @@ import urllib
 from basicutils.chain import *
 from basicutils.network import *
 from basicutils.task import *
-
-
 from mongoengine import *
 
+
 def 约稿(ent: CoreEntity):
-    """#约稿 [#waifu, #召唤]
+    """#约稿 [#waifu, #召唤, #产粮]
     ai画图，txt2img，容易被封所以还是建议优先直接用网页
     """
     ses = requests.session()
@@ -44,62 +46,123 @@ def 约稿(ent: CoreEntity):
         with open(authdir, 'rb') as f:
             usr, pw = pickle.load(f)
         ses.post(apibase+'/login', data={'username':usr,'password':pw})
+    
+    filter_p = re.compile('<.*?>', re.MULTILINE)
+    def filter(src):
+        b = []
+        for i in filter_p.sub(' ', src.lower()).replace('\n', ' ').strip():
+            if b[-1:] == [' '] and ' ' == i:
+                continue
+            else:
+                b.append(i)
+        return ''.join(b)
 
-    tokens = ent.chain.tostr().lower().strip()
-    if tokens == '样例':
+    def parser(src):
+        pm = [
+            'prompt:', 'negative prompt:', 'steps:', 'sampler:', 'cfg scale:',
+            'seed:', 'size:', 'model hash:', 'denoising strength:', 'clip skip:',
+        ]
+        b = [[] for i in pm]
+
+        cur = b[0]
+        p = 0
+
+        def chk():
+            for ind, token in enumerate(pm):
+                if src[p:p+len(token)] == token:
+                    p += len(token)
+                    cur = b[ind]
+                    return False
+            return True
+
+        while p < len(src):
+            if chk():
+                cur.append(src[p])
+                p += 1
+        for p, i in enumerate(b):
+            b[p] = ''.join(i).strip().removesuffix(',')
+        return {j:i for i, j in zip(b, pm) if i}
+
+
+
+        
+    rawinputs = ent.chain.tostr()
+    if rawinputs == '样例':
         with open('Assets/waifusd/prompts.pickle', 'rb') as f:
             li = pickle.load(f)
         return random.choice(li)
-    if tokens == '词汇表':
+    if rawinputs == '词汇表':
         with open('Assets/waifusd/chat_tokens.txt', 'r', encoding='utf-8') as f:
             li = f.read().split('\n')
         return '\n'.join(random.sample(li, 10))
-    neg = ""
+    if rawinputs == '模板':
+        return """((masterpiece)), best quality, illustration, 1 girl, beautiful,beautiful detailed sky, catgirl,beautiful detailed water, cinematic lighting, Ice Wings, (few clothes),loli,(small breasts),light aero blue hair, Cirno(Touhou), wet clothes,underwater,hold breath,bubbles,cat ears ,dramatic angle
+Negative prompt: lowres, bad anatomy, bad hands, text, error, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality, normal quality, jpeg artifacts, signature, watermark, username, blurry, bad feet, huge breasts
+Steps: 75, Sampler: DDIM, CFG scale: 11, Seed: 3323485853, Size: 512x768, Model hash: e6e8e1fc, Clip skip: 2"""
 
-    if 'negative prompt:' in tokens:
-        tokens, neg = tokens.split('negative prompt:', 1)
-    tokens = '((masterpiece)), best quality, illustration, beautiful, beautiful detailed eyes,' + tokens
-    neg += 'nsfw, lowres, bad anatomy, bad hands, text, error, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality, normal quality, jpeg artifacts, signature, watermark, username, blurry'
-    model = [
-        tokens,     # prompt
-        neg,        # negetive prompt
-        "None",
-        "None",
-        20,         # sampling Steps
-        "DDIM",  # sampling method: Euler a, Euler, LMS, Heun, DPM2, DPM2 a, DPM fast, DPM adaptive, DDIM, PLMS
-        False,
-        False,
-        1,          # batch count
-        1,          # batch size
-        7,          # CFG Scale
-        -1,         # seed
-        -1,
-        0,
-        0,
-        0,
-        False,
-        ent.meta.get('-width', 512),        # width
-        ent.meta.get('-height', 512),        # height
-        False,
-        False,
-        0.7,
-        "None",
-        False,
-        False,
-        None,
-        "",
-        "Seed",
-        "",
-        "Steps",
-        "",
-        True,
-        False,
-        None,
-        "",
-        ""
-    ]
-    r = ses.post(f"{apibase}/api/predict", json={'fn_index':12, 'data':model}).json()['data'][0][0][22:]
-    return [Image(base64=r)]
+    txt2img_inputs = collections.namedtuple('txt2img_inputs',
+        field_names=[
+            "prompt", "negative_prompt", "prompt_style", "prompt_style2", "steps",
+            "sampler", "restore_faces", "tiling", "batch_count", "batch_size",
+            "cfg_scale", "seed", "sub_seed", "subseed_strength", "seed_resize_from_h",
+            "seed_resize_from_w", "unk_1", "width", "height", "highres_fix",
+            "scale_latent", "denoising_strength", "script",
+        ], defaults=[
+            "loli", "nsfw", "None", "None", 30,
+            "DDIM", False, False, 1, 1,
+            7, -1, -1, 0, 0,
+            0, False, 512, 512, False,
+            False, 0.85, "None",
+        ])
+
+    def nums(src):
+        b = []
+        for i in src:
+            if i in string.digits+'-':
+                b.append(i)
+        return int(''.join(b))
+    def floats(src):
+        b = []
+        for i in src:
+            if i in string.digits+'.':
+                b.append(i)
+        return float(''.join(b))
+    
+    mapping_string = { # 迫真萃取
+        float: floats,
+        int: nums,
+        str: lambda x: x
+    }
+    
+    parsed = parser(filter(rawinputs))
+    # model = txt2img_inputs()
+    if len(parsed) > 1: # 有东西，高级模式
+        modify = {}
+        for k, v in parsed.items():
+            pk = k[:-1].replace(' ', '_')
+            if pk in txt2img_inputs._fields:
+                modify[pk] = mapping_string[type(txt2img_inputs._field_defaults[pk])](v)
+
+        if sz := parsed.get('size:', ''):
+            w, h = re.compile('([0-9]+)[x\*]([0-9]+)').search(sz).groups()
+            modify['width'] = int(w)
+            modify['height'] = int(h)
+        model = txt2img_inputs(**modify)
+    else: # 简易模式
+        model = txt2img_inputs(
+            prompt='((masterpiece)), best quality, illustration, beautiful, beautiful detailed eyes,' + parsed['prompt:'],
+            negative_prompt='nsfw, lowres, bad anatomy, bad hands, text, error, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality, normal quality, jpeg artifacts, signature, watermark, username, blurry'
+        )
+       
+    args = model + (
+        False, False, None, "", "Seed",
+        "", "Nothing", "", True, False,
+        False, None,
+        "", # json like object
+        ""  # html like object
+    )
+    j = ses.post(f"{apibase}/api/predict", json={'fn_index':13, 'data':args}).json()['data']
+    return [Image(base64=j[0][0][22:]), Plain(filter(j[2]))]
     
 
 
--- testinvoke.py
+++ testinvoke.py
@@ -1,82 +1,126 @@
-from typing import List
-from basicutils.applications.Credit import generate_sign_log, gen_fortune
-from fapi.models.Routiner import imaseconds
-import datetime
-from PIL import Image as PImage
-from PIL import ImageDraw, ImageFont
-from basicutils.media import pimg_base64
+import pickle, random, collections, string, re
 
-rep = generate_sign_log(5)
+def func(rawinputs: str):
+    """#约稿 [#waifu, #召唤, #产粮]
+    ai画图，txt2img，容易被封所以还是建议优先直接用网页
+    """
 
-print(rep)
-import random
-from PIL import Image as PImage
-from PIL import ImageDraw, ImageFont
-# from basicutils.media import pimg_base64
-tegaki_zatsu = 'Assets/851tegaki_zatsu_normal_0883.ttf'
-seto_font = 'Assets/setofont.ttf'
+    filter_p = re.compile('<.*?>', re.MULTILINE)
+    def filter(src):
+        b = []
+        for i in filter_p.sub(' ', src.lower()).replace('\n', ' ').strip():
+            if b[-1:] == [' '] and ' ' == i:
+                continue
+            else:
+                b.append(i)
+        return ''.join(b)
 
-font_tegaki = ImageFont.truetype(tegaki_zatsu, 24)
+    def parser(src):
+        pm = [
+            'prompt:', 'negative prompt:', 'steps:', 'sampler:', 'cfg scale:',
+            'seed:', 'size:', 'model hash:', 'denoising strength:', 'clip skip:',
+        ]
+        b = [[] for i in pm]
 
-fortune_size = 80
-font_fortune = ImageFont.truetype(seto_font, fortune_size)
-rp_size = 20
-font_rp = ImageFont.truetype(seto_font, rp_size)
+        cur = b[0]
+        p = 0
 
-font_yj = ImageFont.truetype(seto_font, 40)
+        def chk():
+            nonlocal p
+            nonlocal cur
+            for ind, token in enumerate(pm):
+                if src[p:p+len(token)] == token:
+                    p += len(token)
+                    cur = b[ind]
+                    return False
+            return True
 
-emotion_type = 'B' if rep.rp >= 62.5 else ('C' if rep.rp <= 47.5 else 'N')
-time_now = imaseconds()
-time_period = 'light' if time_now > 6 * 3600 else ('normal' if 19*3600 > time_now > 12*3600 else 'dark')
+        while p < len(src):
+            if chk():
+                cur.append(src[p])
+                p += 1
+        for p, i in enumerate(b):
+            b[p] = ''.join(i).strip().removesuffix(',')
+        return {j:i for i, j in zip(b, pm) if i}
+        
+    if rawinputs == '样例':
+        with open('Assets/waifusd/prompts.pickle', 'rb') as f:
+            li = pickle.load(f)
+        return random.choice(li)
+    if rawinputs == '词汇表':
+        with open('Assets/waifusd/chat_tokens.txt', 'r', encoding='utf-8') as f:
+            li = f.read().split('\n')
+        return '\n'.join(random.sample(li, 10))
+    if rawinputs == '模板':
+        return """((masterpiece)), best quality, illustration, 1 girl, beautiful,beautiful detailed sky, catgirl,beautiful detailed water, cinematic lighting, Ice Wings, (few clothes),loli,(small breasts),light aero blue hair, Cirno(Touhou), wet clothes,underwater,hold breath,bubbles,cat ears ,dramatic angle
+Negative prompt: lowres, bad anatomy, bad hands, text, error, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality, normal quality, jpeg artifacts, signature, watermark, username, blurry, bad feet, huge breasts
+Steps: 75, Sampler: DDIM, CFG scale: 11, Seed: 3323485853, Size: 512x768, Model hash: e6e8e1fc, Clip skip: 2"""
 
-template = PImage.open(f'Assets/sign/alpha/small/{emotion_type}{time_period}.png').convert('RGBA')
+    txt2img_inputs = collections.namedtuple('txt2img_inputs',
+        field_names=[
+            "prompt", "negative_prompt", "prompt_style", "prompt_style2", "steps",
+            "sampler", "restore_faces", "tiling", "batch_count", "batch_size",
+            "cfg_scale", "seed", "sub_seed", "subseed_strength", "seed_resize_from_h",
+            "seed_resize_from_w", "unk_1", "width", "height", "highres_fix",
+            "scale_latent", "denoising_strength", "script",
+        ], defaults=[
+            "loli", "nsfw", "None", "None", 30,
+            "DDIM", False, False, 1, 1,
+            7, -1, -1, 0, 0,
+            0, False, 512, 512, False,
+            False, 0.85, "None",
+        ])
 
-backgroundRGB = random.randint(0,255), random.randint(0,255), random.randint(0,255), 255
-background_grey = backgroundRGB[0] * 0.299 + backgroundRGB[1] * 0.587 + backgroundRGB[2] * 0.114
+    def nums(src):
+        b = []
+        for i in src:
+            if i in string.digits+'-':
+                b.append(i)
+        return int(''.join(b))
+    def floats(src):
+        b = []
+        for i in src:
+            if i in string.digits+'.':
+                b.append(i)
+        return float(''.join(b))
+    
+    mapping_string = { # 迫真萃取
+        float: floats,
+        int: nums,
+        str: lambda x: x
+    }
+    
+    parsed = parser(filter(rawinputs))
+    if len(parsed) > 1: # 有东西，高级模式
+        modify = {}
+        for k, v in parsed.items():
+            pk = k[:-1].replace(' ', '_')
+            if pk in txt2img_inputs._fields:
+                modify[pk] = mapping_string[type(txt2img_inputs._field_defaults[pk])](v)
 
-layer3 = PImage.new('RGBA',template.size,backgroundRGB) # 上底色
-w, h = template.size
+        if sz := parsed.get('size:', ''):
+            w, h = re.compile('([0-9]+)[x\*]([0-9]+)').search(sz).groups()
+            modify['width'] = int(w)
+            modify['height'] = int(h)
+        model = txt2img_inputs(**modify)
+    else: # 简易模式
+        model = txt2img_inputs(
+            prompt='((masterpiece)), best quality, illustration, beautiful, beautiful detailed eyes,' + parsed['prompt:'],
+            negative_prompt='nsfw, lowres, bad anatomy, bad hands, text, error, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality, normal quality, jpeg artifacts, signature, watermark, username, blurry'
+        )
+    args = model + (
+        False, False, None, "", "Seed",
+        "", "Nothing", "", True, False,
+        False, None,
+        "", # json like object
+        ""  # html like object
+    )
+    return args
 
-font_color = (255, 255, 255, 255) if background_grey < 128 else (0, 0, 0, 255)
+def banner(): print("==========================")
 
-template = PImage.alpha_composite(layer3,template,)
-
-layer2 = PImage.new('RGBA',template.size, (0, 0, 0, 0))
-draw = ImageDraw.Draw(layer2)
-
-text = rep.msg
-fortune = gen_fortune(rep.rp)
-
-def write_center(text, font, height, percentage=0.5):
-    lines = text.split('\n')
-    W = max(font.getsize(tx)[0] for tx in lines)
-    draw.text(((w-W)*percentage , height), text, fill=font_color, font=font)
-
-write_center(fortune, font_fortune, h * 0.04)
-write_center(f"{rep.rp:.3f}%", font_rp, h * 0.115)
-write_center("宜", font_yj, h * 0.142, 0.5)
-write_center("忌", font_yj, h * 0.310, 0.5)
-
-def write_yj_items(li: List[str], font, begin_height):
-    H = font.getsize(li[0])[1]
-    for i in li:
-        C = i.strip().split('\t')
-        if len(C) == 2:
-            A, B = C
-            draw.text((w * 0.05 , begin_height), A, fill=font_color, font=font)
-            draw.text((w * 0.95 , begin_height), B, fill=font_color, font=font, anchor='ra')
-        else:
-            write_center(C[0], font, begin_height)
-        begin_height += H
-
-write_yj_items(rep.y, font_tegaki, h*0.183)
-write_yj_items(rep.j, font_tegaki, h*0.351)
-
-import re
-def disassemble_msg(token: str, msg):
-    t = re.search(token + r'\n(.*?)\n\n', msg, re.M | re.DOTALL).group(1)
-    return t.split('\n')
-
-disassemble_msg('宜:', rep.msg)
-
-PImage.alpha_composite(template,layer2).show()
+print(func("""robot,blue,gun,death"""))
+banner()
+print(func("""masterpiece, best quility ,official art,extremely detailed CG unity 8k wallpaper,delicate scene,2 young girl,symmetrical docking,hug,beautiful detailed eyes, look at the viewer,small breasts,undressing
+Negative prompt: lowres, bad anatomy, bad hands,text, error, missing fingers,extra digit, fewer digits, cropped, worst quality, low quality, normal quality, jpeg artifacts, signature, watermark, username, blurry,dick,cum
+Steps: 32, Sampler: Heun, CFG scale: 12, Seed: 84833071, Size: 512x768, Model hash: e6e8e1fc, Hypernet: anime_3, Clip skip: 2"""))
