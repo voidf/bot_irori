12936f2ff77139b84113f35b03f72adea0ff5e68
ATRI <A@T.RI>
2023-11-16 11:51:41 +0800

    去除消息管理器的自动刷新，拼剪贴板

--- irori_automata.py
+++ irori_automata.py
@@ -1,6 +1,7 @@
 import re
 import os
 import sys
+import datetime
 from typing import List, Tuple
 import aiohttp
 import asyncio
@@ -18,6 +19,8 @@ from basicutils.network import CoreEntity
 from basicutils.chain import MessageChain, Plain, Image
 
 load_dotenv()
+
+QQRichEditFormat = 49769
 
 def read_secret(key: str) -> str:
     v = os.environ[key] = os.environ.get(key) or input(f"Please input {key}:")    
@@ -68,9 +71,6 @@ print(elements)
 print('connect to', elements[0].process_id)
 app = Application(backend="uia").connect(process=elements[0].process_id)
 
-w = app.window(title_re="消息管理器")
-# w.print_control_identifiers()
-
 def get_child(w, idx):
     chl = w.children()
     # print(chl)
@@ -84,13 +84,6 @@ def get_sub(w):
     # child_spec.print_control_identifiers()
     child.click_input() # 模拟鼠标点击
     return child
-
-def click_refresh_btn(w):
-    sonlist = [2, 0, 1, 0, 2, 0, 1, 0]
-    child = get_child(w, sonlist[0])
-    for x in sonlist[1:]:
-        child = get_child(child, x)
-    child.click_input() # 模拟鼠标点击
 
 pat = re.compile(r'^(.*?)(\d{1,2}):(\d{1,2}):(\d{1,2})(.*)$', re.M | re.S)
 pat_qid = re.compile(r'\((\d{1,10})\)$')
@@ -121,16 +114,21 @@ def extract_sender_id(sender_str: str):
             sender = sender_email_found.group(1)
     return sender
 
-def fetch_msg(app):
+async def fetch_msg(app):
     w = app.window(title_re=read_secret('WINDOW_TITLE'))
     li = w.child_window(title_re='消息', control_type='List')
     li.click_input()
+    await asyncio.sleep(0.3)
+    li.click_input()
+    await asyncio.sleep(0.3)
     li.type_keys('^a')
+    await asyncio.sleep(0.3)
     li.type_keys('^c')
+    await asyncio.sleep(0.3)
     data_map = get_clipboard_data()
     pending_msg = []
 
-    for sender, sender_time_str, msgchain in parse_rtf(data_map[49769].decode('utf-8')):
+    for sender, sender_time_str, msgchain in parse_rtf(data_map[QQRichEditFormat].decode('utf-8')):
         sender_id = extract_sender_id(sender)
         if sender_id != read_secret("BOT_ID"): # 屏蔽自己
             if insert_msg(sender_id + '-' + sender_time_str):
@@ -138,55 +136,10 @@ def fetch_msg(app):
     return pending_msg
 
 
-def send_msg(app, t: str = 'test'):
-    w = app.window(title_re=read_secret('WINDOW_TITLE'))
-    edit_ctrl = w.child_window(title="输入", control_type="Edit")
-    edit_ctrl.click_input()
-    wcb.OpenClipboard()  # 打开剪贴板
-    wcb.EmptyClipboard()  # 清空剪贴板
-    wcb.SetClipboardData(win32con.CF_UNICODETEXT, t)  # 设置剪贴板数据
-    wcb.CloseClipboard()  # 关闭剪贴板
-    # pyperclip.copy(t)
-    edit_ctrl.type_keys("^v")
-    # edit_ctrl.type_keys(t, with_spaces=True, with_newlines=True)
-    w.child_window(title="发送(&S)", control_type="Button").click_input()
-
-async def main():
-    login_token = read_secret('LOGIN_TOKEN')
-    register_player_id = read_secret('REG_PID')
-    fetch_msg(w)
-    async with aiohttp.ClientSession() as session:
-        async with session.ws_connect(read_secret("WS_URL").format(login_token, register_player_id)) as ws:
-            async def on_recv_msg(msg):
-                async for msg in ws:
-                    if msg.type == aiohttp.WSMsgType.TEXT:
-                        print(f"Received message: {msg.json()}")
-                        send_msg(app, msg.data)
-                    elif msg.type == aiohttp.WSMsgType.CLOSED:
-                        print('ws closed', msg)
-                        break
-                    elif msg.type == aiohttp.WSMsgType.ERROR:
-                        print('ws error', msg)
-                        break
-                    else:
-                        print("?", msg.type, msg.data)
-            async def loop_scan_msg():
-                while 1:
-                    await asyncio.sleep(0.1)
-                    pending_msg = fetch_msg(w)
-                    for sender, msg in pending_msg:
-                        await ws.send_json({
-                            'chain':msg,
-                            'mid':sender
-                        })
-            scan_task = asyncio.create_task(loop_scan_msg())
-            await on_recv_msg(ws)
-            scan_task.cancel()
-            exit(1)
-
 sender_pat = re.compile(r'(.*?) (20\d{2}/\d{1,2}/\d{1,2} \d{1,2}:\d{1,2}:\d{1,2})$')
 
 def parse_rtf(data: str) -> List[Tuple[str, str, MessageChain]]:
+    print(datetime.datetime.now(), 'bytes:', len(data))
     root = ET.fromstring(data)
     ret = []
     current_sender = None
@@ -208,8 +161,66 @@ def parse_rtf(data: str) -> List[Tuple[str, str, MessageChain]]:
         elif element_type == '1':  # 图片
             buf.append(Image(path=element.get('filepath')))
     if current_sender and buf:
-        ret.append((current_sender, current_send_time, buf))
+        ret.append((current_sender, current_send_time, MessageChain.auto_make(buf)))
     return ret
+
+def dump_rtf(msgchain: MessageChain):
+    out = []
+    for elem in msgchain:
+        if isinstance(elem, Plain):
+            out.append('<EditElement type="0"><![CDATA[' + elem.text + ']]></EditElement>')
+        elif isinstance(elem, Image):
+            out.append('<EditElement type="1" imagebiztype="0" textsummary="" filepath="' + elem.path + '" shortcut=""></EditElement>')
+    wcb.OpenClipboard()  # 打开剪贴板
+    wcb.EmptyClipboard()  # 清空剪贴板
+    wcb.SetClipboardData(QQRichEditFormat, '<QQRichEditFormat><Info version="1001"></Info>' + ''.join(out) + '</QQRichEditFormat>')  # 设置剪贴板数据
+    wcb.CloseClipboard()  # 关闭剪贴板
+
+def send_msg(app, msgchain: MessageChain):
+    w = app.window(title_re=read_secret('WINDOW_TITLE'))
+    edit_ctrl = w.child_window(title="输入", control_type="Edit")
+    edit_ctrl.click_input()
+    dump_rtf(msgchain)
+    edit_ctrl.type_keys("^v")
+    # edit_ctrl.type_keys(t, with_spaces=True, with_newlines=True)
+    w.child_window(title="发送(&S)", control_type="Button").click_input()
+
+async def main():
+    login_token = read_secret('LOGIN_TOKEN')
+    register_player_id = read_secret('REG_PID')
+    await fetch_msg(app)
+    async with aiohttp.ClientSession() as session:
+        async with session.ws_connect(read_secret("WS_URL").format(login_token, register_player_id)) as ws:
+            async def on_recv_msg(msg):
+                async for msg in ws:
+                    if msg.type == aiohttp.WSMsgType.TEXT:
+                        j = msg.json()
+                        print(f"Received message: {j}")
+                        msgchain = MessageChain.parse_obj(j['chain'])
+                        send_msg(app, msgchain)
+                    elif msg.type == aiohttp.WSMsgType.CLOSED:
+                        print('ws closed', msg)
+                        break
+                    elif msg.type == aiohttp.WSMsgType.ERROR:
+                        print('ws error', msg)
+                        break
+                    else:
+                        print("?", msg.type, msg.data)
+            async def loop_scan_msg():
+                while 1:
+                    await asyncio.sleep(3)
+                    pending_msg = await fetch_msg(app)
+                    for sender, msg in pending_msg:
+                        print(sender, msg)
+                        await ws.send_json({
+                            'chain':msg,
+                            'mid':sender
+                        })
+            scan_task = asyncio.create_task(loop_scan_msg())
+            await on_recv_msg(ws)
+            scan_task.cancel()
+            exit(1)
+
 
 # from pathlib import Path
 # {"chain":[{"type":"Plain","meta":{"delay":0},"text":"[4, 12, '*']\n(4*12) = 48"}]}
